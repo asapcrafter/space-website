0743fc8dea21006a7a09ef4dfa10e0f84021a744
Steve <asapcrafter@gmail.com>
2021-09-16 12:38:27 -0700

    minor styling updates

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div id='home'>
+            HELLO
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo cupiditate, mollitia possimus amet sapiente molestiae vel officia doloribus doloremque! Repudiandae et accusamus distinctio nesciunt laborum vel reiciendis impedit? Itaque laborum libero corporis, in quaerat nemo veniam illum quibusdam vel cumque aut ab? Omnis deleniti autem impedit illo, quos natus pariatur. Modi saepe maxime unde similique asperiores. Quis, consectetur commodi natus, possimus quia tempore ea voluptas aliquid iste beatae aspernatur temporibus dicta! Voluptatum blanditiis soluta facilis vel fuga quos omnis magnam laboriosam facere! Blanditiis, deserunt. Molestiae id voluptates ratione doloribus adipisci reprehenderit amet quos tenetur sapiente tempora! Et reprehenderit doloribus quibusdam.
+        </div>
+    );
+}
+
+export default Home;
--- src/components/Stars.js
+++ src/components/Stars.js
@@ -9,9 +9,9 @@ extend({OrbitControls});
 const Points = () => {
     const imgTexture = useLoader(THREE.TextureLoader, circleImage);
 	const count = 5000
-
+    
 	const positions = useMemo(() => {
-		let positions = []
+        let positions = []
         // Position values are randomized 
         for (let i = 0; i < count; i++) {
             const x = Math.random() * 200 - 150 ;
@@ -21,6 +21,20 @@ const Points = () => {
         }
 		return new Float32Array(positions);
 	}, [count])
+     
+    const bufferRef = useRef();
+
+    useFrame(() => {
+        const particle = bufferRef.current.array
+        let light = 0
+
+        for (let i = 0; i < particle.length ; i++) {
+            const currentStar = particle[i];
+            light > 0 ? light = 0 : light++ ;
+            currentStar.material.color = new THREE.Color('hsl(255, 100%, " + lightness + "%)');
+        }
+        
+    })
 
     return (
         <points>
@@ -47,8 +61,8 @@ const Points = () => {
 
 const CameraControls = () => {
     const {camera, gl: {domElement},} = useThree()
-
     const controls = useRef();
+
     useFrame(() => controls.current.update());
 
     return (
@@ -61,14 +75,19 @@ const CameraControls = () => {
     );
 }
 
+const ScrollCamera = () => {
+    const {camera} = useThree()
+    const t = document.body.getBoundingClientRect().top;
+
+    camera.position.y = t * -0.01;
+
+    return null;
+}
+
 const SkyBox = () => {
     const {scene} = useThree();
-
     // Dark blue-black background color
     scene.background = new THREE.Color('#0c0f18')
-
-    // White ambient lighting color
-    // scene.add(new THREE.AmbientLight(0x555555));
     
     return null;
 }
@@ -82,60 +101,65 @@ const Nebula = () => {
 
     // Nebula cloud texture 
     loader.load('/assets/smoke.png', function(texture) {
-        const cloudGeo = new THREE.PlaneBufferGeometry(170, 170);
+        const cloudGeo = new THREE.PlaneBufferGeometry(220, 220);
         const cloudMaterial = new THREE.MeshLambertMaterial({
             transparent: true,
             map: texture,
         });
 
-        for (let i= 0; i < 40; i++) {
+        for (let i= 0; i < 35; i++) {
             const cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
+            const {rotation, material} = cloud;
 
             cloud.position.set(
-            //   Math.random() * 150 - 300, // -300 ~ -150
-              -300 + (i * 3), 
+              -250 + (i * 0.5), 
               Math.random() * 260 - 150, // 200 ~ 350
-              Math.random() * 700 - 300, // -300 ~ 400
+              Math.random() * 700 - 380, // -300 ~ 400
             );
 
-            cloud.rotation.x = 1.16;
-            // cloud.rotation.x = 2;
-            // cloud.rotation.y = -0.12;
-            cloud.rotation.y = (Math.PI / 180) * 90
-            cloud.rotation.z = Math.random() * 2 * Math.PI;
+            rotation.x = 1.16;
+            rotation.y = (Math.PI / 180) * 90
+            rotation.z = Math.random() * 2 * Math.PI;
 
-            cloud.material.opacity = 0.45
-            cloud.renderOrder = 0.5
-            cloud.material.polygonOffset= true
-            cloud.material.polygonOffsetFactor= -4
-            cloud.material.depthTest = false
+            material.opacity = 0.45
+            material.polygonOffset= true
+            material.polygonOffsetFactor= -4
+            material.depthTest = false
 
             cloudParticles.push(cloud);
             scene.add(cloud);
         };
     });  
 
-    // Animates cloud rotation
+    // Animation for cloud rotation
     useFrame(() => {
-        cloudParticles.forEach(p => p.rotation.z -= 0.0011);
+        cloudParticles.forEach(p => p.rotation.z -= 0.0015);
     })
 
     // Lighting
-    const directionalLight = new THREE.DirectionalLight(0x1e326f);
-    directionalLight.position.set(0,0,1);
+    const directionalLight = new THREE.DirectionalLight(0x0c0f18);
+    directionalLight.position.set(-220, 0, 10);
     scene.add(directionalLight);
 
-    const orangeLight = new THREE.PointLight(0x2334a9, 50, 100, 1.7);
-    orangeLight.position.set( -230, 20, 280);
+    const orangeLight = new THREE.PointLight(0x2334a9, 5, 100, 1.7);
+    orangeLight.position.set( -210, 20, 250);
     scene.add(orangeLight);
 
-    const redLight = new THREE.PointLight(0x36336a, 30, 100, 1.7);
-    redLight.position.set( -200, 30, -100);
+    const redLight = new THREE.PointLight(0x36336a, 5, 120, 1.7);
+    redLight.position.set( -210, 30, -180);
     scene.add(redLight);
 
-    const blueLight = new THREE.PointLight(0x1a2e69, 40, 100, 1.7);
-    blueLight.position.set( -200, 80, 100);
+    const blueLight = new THREE.PointLight(0x1a2e69, 5, 120, 1.7);
+    blueLight.position.set( -210, -20, 50);
     scene.add(blueLight);
+
+    const fourLight = new THREE.PointLight(0x353267, 5, 100, 1.7);
+    fourLight.position.set( -210, 80, 350);
+    scene.add(fourLight);
+
+    const fiveLight = new THREE.PointLight(0x353267, 5, 50, 1.7);
+    fiveLight.position.set( -210, 100, -120);
+    scene.add(fiveLight);
 
     return null;
 }
@@ -151,6 +175,7 @@ const Stars = () => {
           {/* <gridHelper args={[200, 50]} /> */}
           <SkyBox />
           <Nebula />
+          <ScrollCamera />
       </Canvas>
     )
   }
