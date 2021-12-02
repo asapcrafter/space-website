9171652ba32199ea6ef6879ae00e474a081d8230
Steve <asapcrafter@gmail.com>
2021-12-01 22:23:20 -0800

    added legal info, more about-section content

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import SpaceshipCanvas from './SpaceshipCanvas.js'
 const Home = () => {
 	// Scrolls to the About section on-click.
 	const handleScrollAbout = () => {
-		document.getElementById('about-title').scrollIntoView({
+		document.getElementById('about-wrapper').scrollIntoView({
 			behavior: 'smooth',
 			block: 'start',
 			inline: 'nearest'
@@ -23,6 +23,11 @@ const Home = () => {
 
 	// Website scrolls to the top on page reload.
 	window.onbeforeunload = setTimeout(window.scrollTo(0, 0), 5000)
+
+	// Returns current year for copyright use.
+	const getYear = () => {
+		return new Date().getFullYear();
+	}
 
 	return (
 		<div id='body'>
@@ -43,12 +48,15 @@ const Home = () => {
 						<p>Welcome to my portfolio.</p>
 						<p>
 							I am a web developer with a passion for designing
-							polished and beautiful applications. My focus is on desktop
-							responsive websites using JavaScript, SASS/CSS, React, and Node.
-							On top of the basic technologies, I love to  experiment
+							polished and beautiful applications. My focus is on creating unique experiences using React, Canvas, and Javascript.
+							On top of the basic technologies, I also love to  experiment
 							in new areas such as 3D graphics using WebGL and ThreeJS.
 						</p>
+						<p>
+							Based in California, I am currently an undergrad at UC Santa Barbara. In my free time, I am always learning new technologies and finding new projects. You can check out some of these ideas down below. 
+						</p>
 					</div>
+
 					{/* <div id='about-tools'>TOOLS</div>
 					<div id='about-tools-container'>
 						<div className='about-tech-container'>
@@ -79,30 +87,33 @@ const Home = () => {
 							</div>
 						</div>
 					</div> */}
+
 					{/* Icon link: https://www.flaticon.com/packs/social-media-171 */}
 					<div id="socials">
-						<a href="https://www.facebook.com/StephenTrieu.t/" target="_blank" rel="noopener noreferrer">
-							<img className="social-logo" src="/assets/facebook-white.png" alt="gh" width="55" height="55"/>
+						<a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+							<img className="social-logo" src="/assets/facebook-white.png" alt="fb" width="50" height="50"/>
 						</a>
 						<a href="https://github.com/asapcrafter" target="_blank" rel="noopener noreferrer">
-							<img className="social-logo" src="/assets/gh.png" alt="gh" width="55" height="55"/>
+							<img className="social-logo" src="/assets/gh.png" alt="gh" width="50" height="50"/>
 						</a>
 						<a href="https://www.linkedin.com/in/stephen-trieu-2170011b8/" target="_blank" rel="noopener noreferrer">
-							<img className="social-logo" src="/assets/linkedin.png" alt="linkedin" width="55" height="55"/>
+							<img className="social-logo" src="/assets/linkedin.png" alt="linkedin" width="50" height="50"/>
+						</a>
+						<a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+							<img className="social-logo" src="/assets/twitter.png" alt="twitter" width="50" height="50"/>
 						</a>
 					</div>
 					<div id="work-wrapper">
 						<div id="work-container">
-							<div id="work-title">WORK</div>
+							<div id="work-title"></div>
 						</div>
 					</div>
 				</div>
 			</div>
-			{/* <Suspense fallback={<div>Loading...</div>}>
-				<SpaceshipCanvas />
-			</Suspense> */}
 			<div id="footer-wrapper">
-				<div id="footer-container"></div>
+				<div id="footer-container">
+					<small>© {getYear()} Stephen Trieu. All Rights Reserved.</small>
+				</div>
 			</div>
 		</div>
 	)
