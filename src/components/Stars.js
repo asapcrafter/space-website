7250a34a0c1404a5887aead98676aeb0602fc968
Steve <asapcrafter@gmail.com>
2021-12-18 17:48:46 -0800

    refactored cloud component

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -11,8 +11,16 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Twinkle from './Twinkle.js'
 import Nebula from './Nebula.js'
+import Nebula2 from './Nebula2.js'
+import smoke from '../assets/smoke.png'
+
 
 extend({ OrbitControls })
+
+// Number generator for object positioning
+const getRandomNumber = (min, max) => {
+	return Math.random() * (max - min) + min;
+}
 
 // Renders non-animated star objects in the scene
 const Points = () => {
@@ -23,9 +31,6 @@ const Points = () => {
 		let positions = []
 
 		// Position values are randomized
-		const getRandomNumber = (min, max) => {
-			return Math.random() * (max - min) + min;
-		}
 		for (let i = 0; i < count; i++) {
 			const x = getRandomNumber(-260, 55) //Outward-inward
 			const y = getRandomNumber(-160, 160) //Down-up
@@ -192,6 +197,46 @@ const SkyBox = () => {
 // })
 // renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+const Cloud = () => {
+	const ref = useRef(null)
+	const texture = useLoader(THREE.TextureLoader, smoke)
+
+	const meshPosition = [
+		getRandomNumber(-235, -250), // -250 ~ -220
+		Math.random() * 260 - 150, // 200 ~ 350
+		Math.random() * 700 - 380 // -300 ~ 400
+	]
+
+	const meshRotation = [
+		1.16,
+		(Math.PI / 180) * 90,
+		Math.random() * 2 * Math.PI
+	]
+
+	// Cloud animation
+	useFrame(() => {
+	    ref.current.rotation.z -= 0.0016
+	});
+
+	return (
+		<mesh ref={ref} position={meshPosition} rotation={meshRotation}>
+			<planeBufferGeometry
+				attach='geometry'
+				args={[200, 200]}
+			/>
+			<meshLambertMaterial
+				attach='material'
+				map={texture}
+				transparent={true}
+				polygonOffset={true}
+				polygonOffsetFactor={-4}
+				depthWrite={false}
+				opacity={0.7}
+			/>
+		</mesh>
+	);
+}
+
 const Stars = () => {
 	return (
 		<Canvas 
@@ -199,6 +244,7 @@ const Stars = () => {
 			camera={{ position: [100, 5, 7], fov: 80 }}
 		>
 			<Nebula />
+			{/* <Nebula2 /> */}
 			<Points />
 			<SkyBox />
 			{/* <Twinkle />
